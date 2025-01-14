afa9c1c96bc3fe0217b014e7e7ec544255397884
santiago <108626122+marcosantiagomuro@users.noreply.github.com>
2025-01-14 13:31:12 +0100

    adding files

---
+++ DTUPayServer/src/main/java/org/acme/MerchantResources.java
@@ -0,0 +1,31 @@
+package org.acme;
+import java.util.List;
+import org.acme.models.Merchant;
+import org.acme.services.MerchantService;
+import dtu.ws.fastmoney.User;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/merchants")
+public class MerchantResources{
+    MerchantService service = new MerchantService();
+    public record UserAccountId(User user, String accountId) {}
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Merchant> getMerchants() {
+        return service.getMerchants();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void setCustomerBank(UserAccountId userAccountId){
+        User user = userAccountId.user;
+        String accountId = userAccountId.accountId;
+        service.setMerchant(user,accountId);
+    }
+}
