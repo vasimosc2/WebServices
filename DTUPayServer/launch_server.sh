7ce1a811b364a6c1466caa1a4a1d8442ef665de1
santiago <108626122+marcosantiagomuro@users.noreply.github.com>
2025-01-14 23:32:35 +0100

    added mvn package

--- DTUPayServer/launch_server.sh
+++ DTUPayServer/launch_server.sh
@@ -5,6 +5,8 @@ set -e  # Exit immediately if a command exits with a non-zero status
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
+mvn package
+
 # Step 1: Build the Docker image using docker-compose
 echo "Building Docker image..."
 docker compose build
