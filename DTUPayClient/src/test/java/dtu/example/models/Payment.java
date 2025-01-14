cf15b012d1a7b017d46ea0feb579ce205276d8a2
santiago <108626122+marcosantiagomuro@users.noreply.github.com>
2025-01-14 17:02:56 +0100

    part 2 of added superclass Stakeholder instead of using bank class USER added lombok for autogenerated setters and getters

--- DTUPayClient/src/test/java/dtu/example/models/Payment.java
+++ DTUPayClient/src/test/java/dtu/example/models/Payment.java
@@ -1,36 +1,16 @@
 package dtu.example.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Payment {
     private int amount;
     private String customerId;
     private String merchantId;
     
     public Payment() {}
-
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-    public String getMerchantId() {
-        return merchantId;
-    }
-
-    public void setMerchantId(String merchantId) {
-        this.merchantId = merchantId;
-    }
 
     public Payment(int amount, String customerId, String merchantId) {
         this.amount = amount;
