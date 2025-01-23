095d2e5fd948c7a5a22d8cf0ec117a2dc7f1412a
santiago <108626122+marcosantiagomuro@users.noreply.github.com>
2025-01-23 15:20:24 +0100

    added service test for reporting-service

--- reporting-service/src/test/java/cucumber/ReportingSteps.java
+++ reporting-service/src/test/java/cucumber/ReportingSteps.java
@@ -5,6 +5,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import models.MoneyTransferredObject;
 import models.Payment;
+import models.PaymentCustomer;
+import models.PaymentMerchant;
 import services.ReportService;
 
 import java.math.BigDecimal;
@@ -13,14 +15,16 @@ import java.util.List;
 public class ReportingSteps {
 
     ReportService reportService = new ReportService();
-    List<Payment> listPaymentsMAnager;
+    List<Payment> listPaymentsManager;
+    List<PaymentCustomer> listPaymentsCustomer;
+    List<PaymentMerchant> listPaymentsMerchant;
+
 
     @Given("a list of transactions saved in the repository")
     public void aListOfTransactionsSavedInTheRepository() {
-
         MoneyTransferredObject moneyTransferredObject1 = new MoneyTransferredObject("token1", "CUST-1", "MERC-1", BigDecimal.valueOf(2.2));
 
-        MoneyTransferredObject moneyTransferredObject2 = new MoneyTransferredObject("token2", "CUST-2", "MERC-2", BigDecimal.valueOf(33.3));
+        MoneyTransferredObject moneyTransferredObject2 = new MoneyTransferredObject("token2", "CUST-1", "MERC-2", BigDecimal.valueOf(33.3));
 
         MoneyTransferredObject moneyTransferredObject3 = new MoneyTransferredObject("token3", "CUST-3", "MERC-3", BigDecimal.valueOf(444.4));
 
@@ -36,18 +40,55 @@ public class ReportingSteps {
     @When("a request to see all transactions is made by the manager")
     public void aRequestToSeeAllTransactionsIsMadeByTheManager() {
 
-        listPaymentsMAnager = reportService.getManagerPaymentReport();
+        listPaymentsManager = reportService.getManagerPaymentReport();
     }
 
     @Then("the list of transactions is shown")
     public void theListOfTransactionsIsShown() {
-        System.out.println("listPaymentsMAnager.size() = " + listPaymentsMAnager.size());
+        System.out.println("listPaymentsMAnager.size() = " + listPaymentsManager.size());
 
-        for ( Payment payment : listPaymentsMAnager) {
-            System.out.println("SANTI payment = " + payment.getCustomerId() + "," + payment.getTokenId() + "," + payment.getMerchantId() + "," + payment.getAmount());
+        for ( Payment payment : listPaymentsManager) {
+            System.out.println("SANTI payment manager = " + payment.getCustomerId() + "," + payment.getTokenId() + "," + payment.getMerchantId() + "," + payment.getAmount());
         }
 
-        assert listPaymentsMAnager.size() == 4;
+        assert listPaymentsManager.size() == 4;
+
+        reportService.clear();
     }
 
+    @When("a request to see all transactions is made by the customer {string}")
+    public void aRequestToSeeAllTransactionsIsMadeByTheCustomer(String arg0) {
+        listPaymentsCustomer = reportService.getCustomerPaymentReport(arg0);
+    }
+
+    @Then("the list of transactions for the customer is shown")
+    public void theListOfTransactionsForTheCustomerIsShown() {
+        System.out.println("listPaymentsCustomer.size() = " + listPaymentsCustomer.size());
+
+        for ( PaymentCustomer payment : listPaymentsCustomer) {
+            System.out.println("SANTI payment customer = " + payment.getTokenId() + "," + payment.getMerchantId() + "," + payment.getAmount());
+        }
+
+        assert listPaymentsCustomer.size() == 2;
+
+        reportService.clear();
+
+    }
+
+    @Then("the list of transactions for the merchant is shown")
+    public void theListOfTransactionsForTheMerchantIsShown() {
+        System.out.println("listPaymentsMerchant.size() = " + listPaymentsMerchant.size());
+
+        for ( PaymentMerchant payment : listPaymentsMerchant) {
+            System.out.println("SANTI payment merchant = " + payment.getTokenId() +  "," + payment.getAmount());
+        }
+
+        assert listPaymentsMerchant.size() == 1;
+        reportService.clear();
+    }
+
+    @When("a request to see all transactions is made by the merchant {string}")
+    public void aRequestToSeeAllTransactionsIsMadeByTheMerchant(String arg0) {
+        listPaymentsMerchant = reportService.getMerchantPaymentReport(arg0);
+    }
 }
--- reporting-service/src/test/resources/features/reporting.feature
+++
@@ -1,9 +0,0 @@
-Feature: Reporting Service
-    Scenario: Reporting of transactions manager
-        Given a list of transactions saved in the repository
-        When a request to see all transactions is made by the manager
-        Then the list of transactions is shown
-
-
-
-
