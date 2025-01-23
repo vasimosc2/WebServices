84b49952e6c3d9f26bfc9be4a8311f6d2914d373
santiago <108626122+marcosantiagomuro@users.noreply.github.com>
2025-01-23 13:29:51 +0100

    started adding service test

---
+++ reporting-service/src/test/java/CucumberTest.java
@@ -0,0 +1,12 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin="summary"
+        , publish= false
+        , features = "features"  // directory of the feature files
+        , snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+public class CucumberTest {
+}
