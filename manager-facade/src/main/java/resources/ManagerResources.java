ceb1e31f052b2ba46cbfa5320b909daf8f41a62f
turingzhi <kaizhifan458@gmail.com>
2025-01-24 13:39:48 +0100

    add manager-facade

---
+++ manager-facade/src/main/java/resources/ManagerResources.java
@@ -0,0 +1,36 @@
+package resources;
+import java.util.List;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import messaging.rabbitmq.reporting.ReportingFactory;
+import models.BankPay;
+import models.Merchant;
+import models.PaymentManager;
+import services.ReportingService;
+
+
+@Path("/reports")
+public class ManagerResources{
+   
+
+
+    private final ReportingService reportingService = ReportingFactory.getService();
+    
+ 
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PaymentManager> getReport() throws Exception {
+        return reportingService.sendRetrieveManagerReportEvent();
+    }
+
+
+
+}
